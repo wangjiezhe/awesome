fb3948301b4c24f2d6ef62dd135f79a11b5e2cba
wangjiezhe <wangjiezhe@gmail.com>
2016-01-24 19:56:16 +0800

    Get battery capacity directly from system

--- battery.lua
+++ battery.lua
@@ -20,17 +20,18 @@ function show_battery (widget)
    end
 
    local ac = get_output("/sys/class/power_supply/" .. ac_adapter .. "/online")
-   local ful = get_output("/sys/class/power_supply/" .. bat_adapter .. "/energy_full")
-   local cur = get_output("/sys/class/power_supply/" .. bat_adapter .. "/energy_now")
+   -- local ful = get_output("/sys/class/power_supply/" .. bat_adapter .. "/energy_full")
+   -- local cur = get_output("/sys/class/power_supply/" .. bat_adapter .. "/energy_now")
    local sta = get_output("/sys/class/power_supply/" .. bat_adapter .. "/status")
 
-   local battery = math.floor(cur * 100 / ful)
+   -- local bat = math.floor(cur * 100 / ful)
+   local bat = get_output("/sys/class/power_supply/" .. bat_adapter .. "/capacity")
 
    if ac:match("0") then
       dir = "v"
-      if battery <= 10 then
+      if bat <= 10 then
          naughty.notify({ title = "Battery Waring",
-                          text = "Batter low! Only ".. battery .. "% left!",
+                          text = "Batter low! Only ".. bat .. "% left!",
                           timeout = 5,
                           fg = beautiful.fg_focus,
                           bg = beautiful.bg_focus})
@@ -43,10 +44,10 @@ function show_battery (widget)
       end
    end
 
-   sur = 100 - battery
+   sur = 100 - bat
    fg = gradient(0, 100, sur)
 
-   battery_text = "Bat: " .. dir .. battery .. "%" .. dir
+   battery_text = "Bat: " .. dir .. bat .. "%" .. dir
    battery_markup = "<span foreground='" .. fg .. "' background='black'>" .. battery_text .. "</span>"
 
    widget:set_markup(battery_markup)
