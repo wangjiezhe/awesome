ef07061e4127461550d9eaa7156afabad6d5c2b5
wangjiezhe <wangjiezhe@gmail.com>
2017-02-15 18:22:12 +0800

    Automaximization for gvim does not work; disable it

--- rc.lua
+++ rc.lua
@@ -632,7 +632,7 @@ awful.rules.rules = {
     { rule_any = {
          class = {
             "URxvt",
-            "Gvim",
+            -- "Gvim",
             -- "Emacs",
             "Lxterminal",
             "Roxterm",
