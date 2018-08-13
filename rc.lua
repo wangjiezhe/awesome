f816545c0e9d2f2d6ae424bab37fb3da2cb790cf
wangjiezhe <wangjiezhe@gmail.com>
2018-08-13 21:29:31 +0800

    Update with awesome-git

--- rc.lua
+++ rc.lua
@@ -592,9 +592,21 @@ for i = 1, 10 do
 end
 
 clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ }, 1, function (c)
+        client.focus = c;
+        c:raise()
+    end),
+    awful.button({ modkey }, 1, function (c)
+        client.focus = c
+        c:raise()
+        awful.mouse.client.move(c)
+    end),
+    awful.button({ modkey }, 3, function (c)
+        client.focus = c
+        c:raise()
+        awful.mouse.client.resize(c)
+    end)
+)
 
 -- Set keys
 root.keys(globalkeys)
@@ -863,8 +875,8 @@ client.connect_signal("manage", function (c)
         -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
 
-    if awesome.startup and
-      not c.size_hints.user_position
+    if awesome.startup
+      and not c.size_hints.user_position
       and not c.size_hints.program_position then
         -- Prevent clients from being unreachable after screen count changes.
         awful.placement.no_offscreen(c)
